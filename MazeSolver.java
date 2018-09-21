2ad5bc89835fe2e68a4b3362dcb20e71867f7317
Matias Bonta <bontamatias@gmail.com>
2018-09-21 12:08:31 -0500

    fyntyk8gtmukezethcuk6+58+8+5+656+5

--- MazeSolver.java
+++ MazeSolver.java
@@ -9,42 +9,37 @@ import java.util.LinkedList;
    as a dead end and do not add paths to the stack.
 */
 public class MazeSolver {
-	/**
-     	Traverses a maze, printing out a path to the exit.
-      	@param maze the maze
-      	@param param the row of the starting position
-      	@param param the column of the starting position
-   	*/
-   	public static void solve(Maze maze, int row, int column) {
-     		Stack<Path> s = new Stack<>();
-     		for (Path p : maze.pathsFrom(row, column))
-     			{
-       		 	s.push(p);
-      		}
+    /**
+        Traverses a maze, printing out a path to the exit.
+        @param maze the maze
+        @param param the row of the starting position
+        @param param the column of the starting position
+    */
+    public static void solve(Maze maze, int row, int column) {
+        Stack<Path> s = new Stack<>();
+        for (Path p : maze.pathsFrom(row, column)) {
+            s.push(p);
+        }
 
-      		Path temp;
-		LinkedList<Path> route = new LinkedList<>();
+        Path temp;
+        LinkedList<Path> route = new LinkedList<>();
 
-      		while (!s.isEmpty()) {
-			temp = s.pop();
-			System.out.println(temp);
+        while (!s.isEmpty()) {
+            temp = s.pop();
+            if (!(maze.pathsFrom(temp.getEndingRow(), temp.getEndingColumn()).size() == 0)) route.add(temp);
+            
+            if (maze.isExit(temp.getEndingRow(), temp.getEndingColumn())) {
+                break;
+            }
+            else {
+                for (Path path : maze.pathsFrom(temp.getEndingRow(), temp.getEndingColumn())) {
+                    if (!path.isOpposite(temp)) {
+                        s.push(path);
+                    }
+                }
+            }
+        }
 
-			if (maze.isExit(temp.getEndingRow(), temp.getEndingColumn())) {
-				break;
-			}
-
-			else {
-				for (Path path : maze.pathsFrom(temp.getEndingRow(), temp.getEndingColumn())) {
-					if (!path.isOpposite(temp)) {
-						s.push(path);
-						route.add(path);
-					}
-					
-				}
-				if (maze.pathsFrom(temp.getEndingRow(), temp.getEndingColumn()).size() == 0) route.remove(temp);
-			}
-		}
-
-		for (Path read : route) System.out.println(read);
-	}
+        for (Path read : route) System.out.println(read);
+    }
 }
